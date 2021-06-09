1d159751244eb70c17baa6a888a29b280cbcebcc
Murendeni Mukwevho <mukwevhom@live.com>
2021-06-09 19:20:33 +0200

    Updated Node modules and updated scrapping script

--- scraper.js
+++ scraper.js
@@ -14,17 +14,17 @@ const scraper = async(link_url) => {
 
     await page.goto(link_url);
     await page.addScriptTag({url: 'https://cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js'});
-    await page.waitFor(1000);
+    await page.waitForTimeout(10000);
 
     result = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".gallery-item[class$=GalleryProduct]"))
+        return Array.from(document.querySelectorAll(".gallery-item:not([target='_blank'])"))
             .map(product => ({
-                brand: product.querySelector("div[class$='GalleryProduct'] h2:first-of-type") ? product.querySelector("div[class$='GalleryProduct'] h2:first-of-type").textContent : "",
-                name: product.querySelector("div[class$='GalleryProduct'] h2:last-of-type") ? product.querySelector("div[class$='GalleryProduct'] h2:last-of-type").textContent : "",
+                brand: product.querySelector("div h2:first-of-type") ? product.querySelector("div h2:first-of-type").textContent : "",
+                name: product.querySelector("div h2:last-of-type") ? product.querySelector("div h2:last-of-type").textContent : "",
                 retail: product.querySelector(':scope > div:last-of-type') ? numeral(product.querySelector(':scope > div:last-of-type h2:last-of-type').textContent.replace("R","").replace(/(\r\n|\n|\r)/gm,"")).value() : "",
                 price: product.querySelector(':scope > div:last-of-type') ?  numeral(product.querySelector(`:scope > div:last-of-type h2:${product.textContent.includes('From') ? 'nth-of-type(2)' : 'first-of-type'}`).textContent.replace("R","").replace(/(\r\n|\n|\r)/gm,"")).value() : "R0",
                 savings: product.querySelector('.savings-badge') ? product.querySelector('.savings-badge span:last-of-type').textContent.includes("R") ? numeral(product.querySelector('.savings-badge span:last-of-type').textContent.replace("R","")).value() : product.querySelector('.savings-badge span:last-of-type').textContent : 0,
-                image: product.querySelector('.image[class$="ImageBlock"]') ? getComputedStyle(product.querySelector(".image[class$='ImageBlock']")).backgroundImage.split('"')[1] : "",
+                image: product.querySelector('.image.product-image') ? getComputedStyle(product.querySelector(".image.product-image")).backgroundImage.split('"')[1] : "",
                 url: product.getAttribute('href') ?  product.getAttribute('href') : "",
                 soldout: product.querySelector('.soldOutWrapper') ? true : false
             }));
