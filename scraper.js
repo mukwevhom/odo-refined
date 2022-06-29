1fba1c37b114b462d3d0f8f4594456bb48da0b90
Murendeni Mukwevho <msiphonic@gmail.com>
2022-06-29 19:45:08 +0200

    Updated the OdO products selectors

--- scraper.js
+++ scraper.js
@@ -17,15 +17,15 @@ const scraper = async(link_url) => {
     await page.waitForTimeout(10000);
 
     result = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".gallery-item:not([target='_blank'])"))
+        return Array.from(document.querySelectorAll(".measure-this"))
             .map(product => ({
-                brand: product.querySelector("div h2:first-of-type") ? product.querySelector("div h2:first-of-type").textContent : "",
-                name: product.querySelector("div h2:last-of-type") ? product.querySelector("div h2:last-of-type").textContent : "",
+                brand: product.querySelector("a div h2:first-of-type") ? product.querySelector("div h2:first-of-type").textContent : "",
+                name: product.querySelector("a div h2:last-of-type") ? product.querySelector("div h2:last-of-type").textContent : "",
                 retail: product.querySelector(':scope > div:last-of-type') ? numeral(product.querySelector(':scope > div:last-of-type h2:last-of-type').textContent.replace("R","").replace(/(\r\n|\n|\r)/gm,"")).value() : "",
                 price: product.querySelector(':scope > div:last-of-type') ?  numeral(product.querySelector(`:scope > div:last-of-type h2:${product.textContent.includes('From') ? 'nth-of-type(2)' : 'first-of-type'}`).textContent.replace("R","").replace(/(\r\n|\n|\r)/gm,"")).value() : "R0",
                 savings: product.querySelector('.savings-badge') ? product.querySelector('.savings-badge span:last-of-type').textContent.includes("R") ? numeral(product.querySelector('.savings-badge span:last-of-type').textContent.replace("R","")).value() : product.querySelector('.savings-badge span:last-of-type').textContent : 0,
-                image: product.querySelector('.image') ? getComputedStyle(product.querySelector(".image")).backgroundImage.split('"')[1] : "",
-                url: product.getAttribute('href') ?  product.getAttribute('href') : "",
+                image: product.querySelector('.image') ? product.querySelector(".image").src : "",
+                url: product.querySelector('a').getAttribute('href') ?  product.querySelector('a').getAttribute('href') : "",
                 soldout: product.querySelector('.soldOutWrapper') ? true : false
             }));
     });
